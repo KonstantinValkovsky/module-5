80c675ced96845cbfd523548807c580d4985f8af
Kostya <113539367+KonstantinValkovsky@users.noreply.github.com>
2023-05-05 20:42:13 +0300

    Выполнил 1 задание

--- module 5/Program.cs
+++ module 5/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          (string Name, string[] Dishes ) User;
+
+            Console.Write("Как вас зовут? ");
+            User.Name = Console.ReadLine();
+
+
+            User.Dishes = new string[5];
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.Write("Введите любимое блюдо номер {0}: ", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
